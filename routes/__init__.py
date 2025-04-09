cf39998a42468d0ddfc92f7dcad31e4a6126a86f
m0neZ <ramonkattan@gmail.com>
2025-04-09 14:48:44 -0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,6 +4,7 @@ from routes.supplier_routes import supplier_bp
 from routes.transaction_routes import transaction_bp
 from routes.summary_routes import summary_bp
 from routes.alert_routes import alert_bp
+from .field_routes import field_bp
 
 def register_routes(app):
     app.register_blueprint(product_bp, url_prefix='/api/products')
@@ -11,3 +12,4 @@ def register_routes(app):
     app.register_blueprint(transaction_bp, url_prefix='/api/transactions')
     app.register_blueprint(summary_bp, url_prefix='/api')
     app.register_blueprint(alert_bp, url_prefix='/api/alerts')
+    app.register_blueprint(field_bp, url_prefix='/api/fields')
