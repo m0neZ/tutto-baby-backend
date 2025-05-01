4355c349caffcd0d25f83031963544de5a81a054
m0neZ <ramonkattan@gmail.com>
2025-04-30 22:19:59 -0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from models.product import Product
-from models.supplier import Supplier
-from models.transaction import InventoryTransaction
-from .field_option import FieldOption
